ab79f511bd68015f3a93904632932fd2b0fca176
Ramon Audain <raudain123@gmail.com>
2020-08-23 17:49:38 -0700

    Finish section 5

---
+++ 05. PHP Built-in Functions/mathFunctions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+
+echo pow(2,7) . "<br>"; // 2 to the 7th power
+echo rand(1, 100) . "<br>"; // random
+echo sqrt(100) . "<br>"; // square root
+echo ceil(5.6) . "<br>";
+echo floor(5.6)
+
+
+?>
+
+</body>
+</html>
